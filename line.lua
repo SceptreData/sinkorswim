8b8bcb29a1f213ea3ff6c0264702b4df64e79ad3
David Bergeron <david.f.bergeron@gmail.com>
2017-02-22 14:21:52 -0700

    added calcMidpoint

--- line.lua
+++ line.lua
@@ -1,7 +1,7 @@
 Line = {}
 Line.__index = Line
 
-local function round(...)
+local round = function(...)
   local arg = {...}
   if #arg == 1 then
     local n = arg[1]
@@ -16,23 +16,27 @@ local function round(...)
 end
 
 local iterate_line = function (x0, y0, x1, y1)
-                x0, y0, x1, y1 = round(x0, y0, x1, y1)
-                local dx, dy = math.abs(x1 - x0), -math.abs(y1 - y0)
-                local sx, sy = x0 < x1 and 1 or -1, y0 < y1 and 1 or -1
-                local err, e2 = dx + dy
+   x0, y0, x1, y1 = round(x0, y0, x1, y1)
+   local dx, dy = math.abs(x1 - x0), -math.abs(y1 - y0)
+   local sx, sy = x0 < x1 and 1 or -1, y0 < y1 and 1 or -1
+   local err, e2 = dx + dy
 
-                while (x0 ~= x1 and y0 ~= y1) do
-                    e2 = 2 * err
-                    if e2 >= dy then
-                        err = err + dy
-                        x0 = x0 + sx
-                    end
-                    if e2 <= dx then
-                        err = err + dx
-                        y0 = y0 + sy
-                    end
-                    coroutine.yield(x0, y0)
-                end
+   while (x0 ~= x1 and y0 ~= y1) do
+       e2 = 2 * err
+       if e2 >= dy then
+           err = err + dy
+           x0 = x0 + sx
+       end
+       if e2 <= dx then
+           err = err + dx
+           y0 = y0 + sy
+       end
+       coroutine.yield(x0, y0)
+   end
+end
+
+function Line.midpoint (x0, y0, x1, y1)
+    return (x0 + x1) / 2, (y0 + y1) / 2
 end
 
 function Line.getPoints (x0, y0, x1, y1)
