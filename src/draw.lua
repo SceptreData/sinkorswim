49a79b5cf1b10e7d60a2d97652756b08487e8fc0
David Bergeron <sceptredata@gmail.com>
2017-03-17 05:38:44 +0100

    turns out I didnt actually add the new draw file. woops.

---
+++ src/draw.lua
@@ -0,0 +1,34 @@
+local Animation = require('animation')
+local Vector = require('vec2')
+local Rect = require('rect')
+
+local lg = love.graphics
+
+local Draw = {}
+Draw.__index = Draw
+
+function Draw.animatedSprite(v, x, y)
+  v.animation.cur:draw(v.img, x, y)
+end
+
+function Draw.static(v, x, y)
+  lg.draw(v.img, v.sprite, x, y)
+end
+
+function Draw.empty(x, y)
+  return
+end
+
+function Draw.box(x, y, w, h)
+  local box = Rect(x, y, w, h)
+  box:drawBorder()
+end
+
+function Draw.mapNode(node, map_x, map_y)
+  Draw.box(map_x + ((node:getX()) * 32), map_y + ((node:getY()) * 32), 32, 32)
+end
+
+
+
+return Draw
+
